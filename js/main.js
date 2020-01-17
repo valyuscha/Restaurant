3ebffd5fd4268febcadb500e80c27e76627432dd
Valentine <valyuscha2004@gmail.com>
2020-01-17 21:19:46 +0200

    Made new responsive page

---
+++ js/main.js
@@ -0,0 +1,181 @@
+$('.feature__slider-block').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    speed: 300,
+    arrow: true,
+    prevArrow: $('.feature-prev'),
+    nextArrow: $('.feature-next'),
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 5
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 820,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 570,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 450,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+})
+
+$('.stuff__slider-block').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 300,
+    arrow: true,
+    prevArrow: $('.stuff-prev'),
+    nextArrow: $('.stuff-next'),
+    responsive: [
+        {
+            breakpoint: 1205,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 910,
+            settings: {
+                slidesToShow: 1,
+                style: {
+                    width: '50%'
+                }
+            }
+        }
+    ]
+})
+
+$('.feedback__slider-block').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    arrow: true,
+    prevArrow: $('.feedback-prev'),
+    nextArrow: $('.feedback-next'),
+    responsive: [
+        {
+            // breakpoint: 1199
+        }
+    ]
+})
+
+// $('.blog__slider-block').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     speed: 300,
+//     arrow: true,
+//     prevArrow: $('.blog-prev'),
+//     nextArrow: $('.blog-next'),
+//     responsive: [
+//         {
+//             breakpoint: 1205,
+//             settings: {
+//                 slidesToShow: 2
+//             }
+//         },
+//         {
+//             breakpoint: 830,
+//             settings: {
+//                 slidesToShow: 1
+//             }
+//         }
+//     ]
+// })
+
+
+let slickCenter,
+    blogSlider = $('.blog__slider-block')
+
+blogSlider.on('init', function (event, slick) {
+    slickCenter = $('.slick-center')
+
+    slickCenter.find('section').css({
+        margin: '0 auto',
+        // width: 370
+    })
+
+    slickCenter.prev().find('section').css({
+        margin: '0'
+    })
+
+    slickCenter.next().find('section').css({
+        marginLeft: 'auto'
+    })
+})
+
+blogSlider.slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 300,
+    arrow: true,
+    centerMode: true,
+    centerPadding: 0,
+    prevArrow: $('.blog-prev'),
+    nextArrow: $('.blog-next'),
+    responsive: [
+        {
+            breakpoint: 1150,
+            settings: {
+                centerMode: false
+            }
+        },
+        {
+            breakpoint: 1085,
+            settings: {
+                centerMode: false,
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 750,
+            settings: {
+                centerMode: false,
+                slidesToShow: 1
+            }
+        }
+    ]
+})
+
+blogSlider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    if ((currentSlide > nextSlide && (nextSlide !== 0 || currentSlide === 1)) || (currentSlide === 0 && nextSlide === slick.slideCount - 1)) {
+        slickCenter = $('.slick-center').prev()
+    } else if (currentSlide === nextSlide) {
+        slickCenter = $('.slick-center')
+    } else {
+        slickCenter = $('.slick-center').next()
+    }
+
+    slickCenter.find('section').css({
+        margin: '0 auto',
+    })
+
+    slickCenter.prev().find('section').css({
+        margin: '0'
+    })
+
+    slickCenter.next().find('section').css({
+        marginLeft: 'auto'
+    })
+
+
+})
---
+++ js/script.js
@@ -0,0 +1,142 @@
+// Вывод параграфов в блоке about - начало
+
+const aboutFigures = document.querySelector('.about__block_text_figures')
+
+aboutFigures.addEventListener('click', function (event) {
+
+    const titles = document.querySelectorAll('.about__block_text_figures_title')
+    const icons = document.querySelectorAll('.about-angle')
+    const tagName = event.target.tagName.toLowerCase()
+    let arr
+
+    if (tagName === 'h4') {
+        arr = titles
+    } else if (tagName === 'i') {
+        arr = icons
+    } else {
+        return
+    }
+
+
+    arr.forEach(e => {
+        let item
+        let element
+
+        if (e.id) {
+            item = (e.id === event.target.id)
+            element = e.id
+        } else {
+            const parent = e.parentNode
+            item = (parent.id === event.target.parentNode.id)
+            element = (parent.id)
+        }
+
+        if (item) {
+            const txtStyle = document.getElementById(element).style
+            const nextElementSiblingStyle = document.getElementById(element).nextElementSibling.style
+            const className = document.getElementById(element).lastChild.classList
+
+            if (txtStyle.getPropertyValue("color") === "rgb(0, 197, 210)") {
+                txtStyle.setProperty("color", "#222222")
+                nextElementSiblingStyle.display = 'none'
+                className.remove('fa-angle-up')
+                className.add('fa-angle-down')
+            } else {
+                txtStyle.setProperty("color", "rgb(0, 197, 210)")
+                nextElementSiblingStyle.display = 'block'
+                className.remove('fa-angle-down')
+                className.add('fa-angle-up')
+            }
+        }
+    })
+})
+
+// Вывод параграфов в блоке about - конец
+
+
+// Вывод видео - начало
+
+$('.video-img').click(function () {
+    $('.body__filter').css({
+        display: 'block'
+    })
+    $('.video-iframe').css({
+        display: 'block'
+    })
+})
+
+$('.body__filter').click(function () {
+    $('.body__filter').css({
+        display: 'none'
+    })
+    $('.video-iframe').css({
+        display: 'none'
+    })
+    const stopVideo = $('#video').attr('src')
+    $('#video').attr('src', stopVideo)
+})
+
+// Вывод видео - конец
+
+
+//    Кнопка вверх (.btn_up) Begin
+
+$('body').append('<button class="btn_up" />')
+
+$('.btn_up').click(function () {
+    $('body').animate({
+        'scrollTop': 0
+    }, 1000)
+    $('html').animate({
+        'scrollTop': 0
+    }, 1000)
+})
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 200) {
+        $('.btn_up').addClass('active')
+    } else {
+        $('.btn_up').removeClass('active')
+    }
+})
+
+//    Кнопка вверх (.btn_up) End
+
+
+// Фиксированое меню - начало
+
+$('.menu_links').append('<div class="menu_links">')
+$('.menu_links_block_menu-list').append('<a href="#feature-item">')
+$('.menu_links_block_logo').append('<div class="menu_links_block_logo">')
+$('.mobile_menu_click__link').append('<a class="mobile_menu_click__link color-black" href="#!">')
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 200) {
+        $('.menu_links').removeClass('static')
+        $('.menu_links').addClass('scroll')
+        $('.menu_links_block_menu-list').removeClass('color-black')
+        $('.menu_links_block_menu-list').addClass('color-white')
+        $('.menu_links_block_logo').addClass('display-none')
+        $('.mobile_menu_click__link').removeClass('color-black')
+        $('.mobile_menu_click__link').addClass('color-white')
+    } else {
+        $('.menu_links').removeClass('scroll')
+        $('.menu_links').addClass('static')
+        $('.menu_links_block_menu-list').removeClass('color-white')
+        $('.menu_links_block_menu-list').addClass('color-black')
+        $('.menu_links_block_logo').removeClass('display-none')
+        $('.mobile_menu_click__link').removeClass('color-white')
+        $('.mobile_menu_click__link').addClass('color-black')
+    }
+})
+
+// Фиксированое меню - конец
+
+
+// Меню-бургер - начало
+
+$('.mobile_menu_click').click(function(){
+    $('.menu_links_block_menu').fadeToggle();
+})
+
+// Меню-бургер - конец
